6e2261b234f5ba6a1482732b6bff2426421ae5a7
Will Kotheimer <wkotheimer@gmail.com>
2019-06-09 23:37:15 -0500

    Added filters and map expressions

--- basics/loops.scala
+++ basics/loops.scala
@@ -1,12 +1,34 @@
 object loops {
 	def main(args: Array[String]) {
 	
-	for(i <- 1 to 5) {
+	for(i <- 1.to(5)) {
 		println("i using to "+i);
 	}
 
+	 for(i <- 1.until(5)) {
+                println("i using to "+i);
+        }
 
+	 for(i <- 1 to 5; j <- 1 to 3) {
+                println("i using to "+i+" "+j);
+        }
+
+	//iterate over a list:
+	val lst = List(1,2,3,44,56,67,233)
 	
+	 for(i <- lst) {
+                println("i using to "+i);
+        }
+	//with filters
+	 for(i <- lst; if i%2==0) {
+                println("i using to "+i);
+        }
+	//as expression
+	val result = for {i <- lst; if i%2==0} yield {
+                i*i
+        }
+	println(result);
+
 	  var y=0;
         while (y<10) {
                 println("y = "+y);
