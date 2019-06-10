c3b260eed49962b08ccc3f01ade75dc17fa03520
Will Kotheimer <wkotheimer@gmail.com>
2019-06-09 22:58:17 -0500

    Added loops and control flow

---
+++ basics/ifelse.scala
@@ -0,0 +1,19 @@
+object Demo {
+	
+	def main(args: Array[String]) {
+		val x = 20;
+		var res = "";
+
+		if (x==20) {
+		
+		res="x == 20";
+		} else {
+
+		res= "x != 20";
+
+		}
+	println(res);
+	val res2 = if (x==20) "x==20" else "x!=20"
+	println(res2);
+	}	
+}
---
+++ basics/loops.scala
@@ -0,0 +1,25 @@
+object loops {
+	def main(args: Array[String]) {
+	
+	for(i <- 1 to 5) {
+		println("i using to "+i);
+	}
+
+
+	
+	  var y=0;
+        while (y<10) {
+                println("y = "+y);
+                y+=1;
+        }
+
+
+
+         y=0;
+        do {
+                println("y= "+y);
+                y+=1;
+        } while (y < 10)
+}
+
+}
