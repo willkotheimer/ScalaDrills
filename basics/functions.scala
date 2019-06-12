dcccbfb944726802bfe277d20e5e92c4879b255e
Will Kotheimer <wkotheimer@gmail.com>
2019-06-11 23:00:18 -0500

    Added functions

---
+++ basics/functions.scala
@@ -0,0 +1,35 @@
+//Functions in Scala
+
+object functions {
+
+	object Math {
+		def add(x : Int, y : Int) : Int = {
+			return x +y;
+		}
+
+	}
+
+	def add(x : Int, y : Int) : Int = {
+		return x+y;
+	
+	}
+	def subtract(x : Int, y : Int) : Int = {
+                x-y;
+
+        }
+	def multiply(x : Int, y : Int) : Int = x * y;
+
+ 
+	def divide(x : Int, y : Int) : Int = x / y;
+
+
+	def main(args: Array[String]) {
+		println(add(2,3));
+		println(subtract(2,3));
+		println(multiply(2,3));
+		println(divide(2,3));
+		println(Math.add(5,6));
+
+	}
+
+}
--- basics/matchexpress.scala
+++ basics/matchexpress.scala
@@ -5,6 +5,29 @@ object Match {
 		age match {
 		case 20 => println(age);
 		case 18 => println(age);
+		case 30 => println(age);
+		case 40 => println(age);
+		case 50 => println(age);
+
+		case _=> println("default");
+		}
+
+		val result = age match {
+                case 20 => age;
+                case 18 => age;
+                case 30 => age;
+                case 40 => age;
+                case 50 => age;
+
+		case _=> "default";
+                }
+
+		println("result = "+result);
+		
+		val i = 2;
+		i match {
+			case 1 | 3 | 5 | 7 | 9 => println("odd");
+			case 2 | 4 | 6 | 8 | 10 => println("even")
 		}
 	}
 
